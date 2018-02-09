1200d5f8c0c61fd4bafa16ba15394deacb93c338
zahir yahya <zahir_yahya@yahoo.com>
2018-02-09 14:15:26 -0800

    cleaned code

--- models/user.js
+++ models/user.js
@@ -7,13 +7,10 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var dbstr = 'recipe';
 var connectionString = db_info.LOGIN_STR;
 var db = pgp(connectionString);
 
 module.exports.createUser = function(newUser, callback) {
-	// add user to db
-	console.log("accessing db");
 
 	db.none('insert into users(email, username, password)' +
       'values(${email}, ${username}, ${password})', {
